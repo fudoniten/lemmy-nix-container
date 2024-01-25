bc1c6a195d8c6d28b2c404bb8c9fffca8086b630
niten <niten@fudo.org>
2024-01-25 11:40:50 -0800

    There is no port config clause, attach to hostname.

--- lemmy-container.nix
+++ lemmy-container.nix
@@ -98,8 +98,7 @@ in {
             server.package = cfg.server-package;
             settings = {
               email = {
-                smtp_server = cfg.smtp.host;
-                smtp_port = cfg.smtp.port;
+                smtp_server = "${cfg.smtp.host}:${cfg.smtp.port}";
                 smtp_from_address = "noreply@${cfg.hostname}";
               };
               hostname = cfg.hostname;
